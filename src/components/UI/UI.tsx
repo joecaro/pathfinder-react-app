951f72bd894b61c06a594c826bb3ae20002f69e6
joecaro <jsc11598@gmail.com>
2021-11-10 14:27:21 -0500

    ui-legend

--- src/components/UI/UI.tsx
+++ src/components/UI/UI.tsx
@@ -1,24 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
+import '../PathfindingVisualizer/Node/Node.css'
 
 const UIContainer = styled.div`
+	background-color: #00000022;
+	box-shadow: 0 0 5px #00000066;
 	position: fixed;
 	top: 0;
 	width: 100%;
 	padding: 1rem;
-	display: flex;
+	display: grid;
+	grid-template-columns: 200px 1fr;
 	align-items: center;
-	justify-content: flex-start;
 	h1 {
 		display: inline-block;
-		margin: 0 1rem;
+		margin: auto;
+	}
+	p {
+		font-size: 2rem;
+		display: inline-block;
+		margin-right: 40px;
 	}
 	button {
 		padding: 1rem;
 		border-radius: 5px;
 		border: 1px solid #555;
 		margin: auto 10px;
+		background-color: white;
+		opacity: .9;
+		:hover {
+			opacity: 1;
+		}
 	}
+`
+
+const Square = styled.div`
+	width: 30px;
+	height: 30px;
+	display: inline-block;
 `
 
 interface UIProps {
@@ -35,8 +54,22 @@ export default function UI(props: UIProps) {
 	return (
 		<UIContainer>
 			<h1>UI</h1>
-			<button onClick={runAlgorithm}>Visualize Algorithm</button>
-			<button onClick={handleRefresh}>Reset</button>
+			<div>
+				<div>
+					<button onClick={runAlgorithm}>Visualize Algorithm</button>
+					<button onClick={handleRefresh}>Reset</button>
+				</div>
+				<div>
+					<Square className="node-start"></Square>
+					<p>Starting Cell</p>
+					<Square className="node-end"></Square>
+					<p>End Cell</p>
+					<Square className="node"></Square>
+					<p>Traversable Cell</p>
+					<Square className="node-wall"></Square>
+					<p>Wall</p>
+				</div>
+			</div>	
 		</UIContainer>
 	)
 }
